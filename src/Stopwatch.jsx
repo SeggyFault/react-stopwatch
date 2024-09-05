20c68d97ca74024fd29f16e25c9a5e021d6d8055
sggyflt <sgflt88@gmail.com>
2024-09-05 20:03:39 +0200

    feat: add basic structure

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,8 +1,41 @@
 import {useState, useEffect, useRef} from "react";
 
 function Stopwatch() {
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+
+  }, [isRunning]);
+
+  function start() {
+
+  }
+
+  function stop() {
+
+  }
+
+  function reset() {
+
+  }
+
+  function formatTime() {
+    return `00:00:00`
+  }
+
   return(
-    <div></div>
+    <div>
+      <div>{formatTime()}</div>
+      <div>
+        <button onClick={start}>Start</button>
+        <button onClick={stop}>Stop</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
   )
 }
 
