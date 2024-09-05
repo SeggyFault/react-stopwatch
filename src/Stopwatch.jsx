6777dd5f6ef885cd274b38758b05cfffdd7efbd7
sggyflt <sgflt88@gmail.com>
2024-09-05 20:11:07 +0200

    style: add basic styles

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -28,12 +28,12 @@ function Stopwatch() {
   }
 
   return(
-    <div>
-      <div>{formatTime()}</div>
-      <div>
-        <button onClick={start}>Start</button>
-        <button onClick={stop}>Stop</button>
-        <button onClick={reset}>Reset</button>
+    <div className="flex flex-col h-screen justify-center items-center gap-4">
+      <div className="text-9xl">{formatTime()}</div>
+      <div className="flex gap-2">
+        <button className="px-4 py-2 border border-green-800 rounded-md bg-green-600 hover:bg-green-700 text-white text-3xl" onClick={start}>Start</button>
+        <button className="px-4 py-2 border border-red-800 rounded-md bg-red-600 hover:bg-red-700 text-white text-3xl" onClick={stop}>Stop</button>
+        <button className="px-4 py-2 border border-blue-800 rounded-md bg-blue-600 hover:bg-blue-700 text-white text-3xl" onClick={reset}>Reset</button>
       </div>
     </div>
   )
