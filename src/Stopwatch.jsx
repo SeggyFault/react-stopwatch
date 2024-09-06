1c0d0f3fd0ad7e24e11497e2bdd6bc0936c6b4be
sggyflt <sgflt88@gmail.com>
2024-09-06 10:38:20 +0200

    feat: add stopwatch functionality

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -8,32 +8,53 @@ function Stopwatch() {
   const startTimeRef = useRef(0);
 
   useEffect(() => {
+    if(isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current)
+      }, 10)
+    }
 
+    return () => {
+      clearInterval(intervalIdRef.current);
+    }
   }, [isRunning]);
 
   function start() {
-
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
   }
 
   function stop() {
-
+    setIsRunning(false);
   }
 
   function reset() {
-
+    setElapsedTime(0);
+    setIsRunning(false);
   }
 
   function formatTime() {
-    return `00:00:00`
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / (1000) % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+    
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`
   }
 
   return(
-    <div className="flex flex-col h-screen justify-center items-center gap-4">
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
       <div className="text-9xl">{formatTime()}</div>
       <div className="flex gap-2">
-        <button className="px-4 py-2 border border-green-800 rounded-md bg-green-600 hover:bg-green-700 text-white text-3xl" onClick={start}>Start</button>
-        <button className="px-4 py-2 border border-red-800 rounded-md bg-red-600 hover:bg-red-700 text-white text-3xl" onClick={stop}>Stop</button>
-        <button className="px-4 py-2 border border-blue-800 rounded-md bg-blue-600 hover:bg-blue-700 text-white text-3xl" onClick={reset}>Reset</button>
+        <button className="px-4 py-2 text-3xl text-white bg-green-600 border border-green-800 rounded-md hover:bg-green-700" onClick={start}>Start</button>
+        <button className="px-4 py-2 text-3xl text-white bg-red-600 border border-red-800 rounded-md hover:bg-red-700" onClick={stop}>Stop</button>
+        <button className="px-4 py-2 text-3xl text-white bg-blue-600 border border-blue-800 rounded-md hover:bg-blue-700" onClick={reset}>Reset</button>
       </div>
     </div>
   )
